09fadd46e969a92a950137deb2d1190882eefcac
bbartika <137605070+bbartika@users.noreply.github.com>
2023-12-10 10:51:53 +0530

    Create user.js

---
+++ controller/user.js
@@ -0,0 +1,68 @@
+const User=require('../model/user')
+const bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
+
+
+
+function generateAccessToken(id,name,ispremium){
+    return jwt.sign({userid:id,username:name,isPremium:ispremium},'secretkey')
+}
+
+
+function isStringInvalid(string){
+    if(string===undefined || string.length===0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+
+exports.signUp=async(req,res)=>{
+    try{
+        const {name,email,password}=req.body
+        
+        if(isStringInvalid(name) || isStringInvalid(email) || isStringInvalid(password)){
+            return res.status(400).json({error:"something is missing"})
+        }
+
+        const saltRounds=10
+        const hash = await bcrypt.hash(password, saltRounds);
+
+         await User.create({name,email,password:hash})
+                res.status(200).json("record inserted correctly")
+
+
+    }catch(err){
+        res.status(404).json({error:err })
+
+    }
+   
+}
+
+exports.logIn = async (req, res) => {
+    try {
+        const{useremail,userpassword}  = req.body
+
+        if(isStringInvalid(useremail) || isStringInvalid(userpassword) ){
+            return res.status(400).json({error:"something is missing"})
+        }
+
+        const user = await User.findOne({ where: { email: useremail } });
+        // console.log("user",user)
+
+        if (user) {
+            const passwordMatch = await bcrypt.compare(userpassword, user.password);
+            if (passwordMatch) {
+                res.status(200).json({ message: "User logged in successfully",token:generateAccessToken(user.id,user.name,user.isPremium)});
+            } else {
+                res.status(400).json({ error: 'Invalid password' });
+            }
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
