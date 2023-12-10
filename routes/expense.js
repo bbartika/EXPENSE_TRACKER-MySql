7e59e715e0f5e49234e45da85d3a0cb8e6f2fbe6
bbartika <137605070+bbartika@users.noreply.github.com>
2023-12-10 10:11:38 +0530

    Create expense.js

---
+++ routes/expense.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const router=express.Router()
+const expenseController=require('../controller/expense')
+const userAuthentication=require('../middleware/auth')
+const downloadController=require('../controller/download')
+
+// EXPENSE ROUTES
+router.post('/add-expenses',userAuthentication.authenticate,expenseController.addExpenses)
+router.get('/get-expenses',userAuthentication.authenticate,expenseController.getExpenses )
+router.delete('/delete-expense/:id',userAuthentication.authenticate,expenseController.deleteExpense)
+
+
+//Download  Routes
+
+router.get('/downloadExpenses',userAuthentication.authenticate,downloadController.downloadExpenses)
+router.get('/getdownloadedURLS',userAuthentication.authenticate,downloadController.getURLS)
+
+
+module.exports=router
