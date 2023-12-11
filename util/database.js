44d6c7b79ac783b8d78a07857df287dd505ad62f
bbartika <137605070+bbartika@users.noreply.github.com>
2023-12-11 15:24:50 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,18 +1,12 @@
 const Sequelize = require('sequelize');
 
 
-const sequelize = new Sequelize(
-    
-    website=http://51.20.254.16:3000
-    
-   {
+const sequelize = new Sequelize('expensetracker','mahibaby','uitIT%1822',{
     dialect: 'mysql',
     host: 'database-1.ck4vugbeanlm.eu-north-1.rds.amazonaws.com'
-    username: 'mahibaby'
-    password: 'uitIT%1822'
-    database: 'expensetracker',
-   }
-);
+    
+   })
+
 
 // Rest of your Sequelize setup and model definitions go here
 
