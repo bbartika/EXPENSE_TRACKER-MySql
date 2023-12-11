aa136df4706fd89639d892ccd49f4c331caf6329
bbartika <137605070+bbartika@users.noreply.github.com>
2023-12-11 09:59:21 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,10 +1,14 @@
-const Sequelize = require('sequelize')
-
-
-
-const sequelize = new Sequelize('expense-tracker', 'root', 'uitIT$1822',{
-    dialect: 'mysql',
-    host: 'localhost'
-})
-
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+    process.env.DATABASE_NAME,    
+    process.env.DATABASE_USERNAME, 
+    process.env.DATABASE_PASSWORD, 
+    {
+      dialect: process.env.DATABASE_DIALECT, 
+      host: process.env.DATABASE_HOST,        
+      logging: false
+    }
+  );
+  
+  module.exports = sequelize;
 module.exports = sequelize;
