ab59c7553bf70c197d9402e8bc2cde2781cb5b5d
bbartika <137605070+bbartika@users.noreply.github.com>
2023-12-13 21:03:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ filesDownloaded.belongsTo(user)
 // ...
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
+    app.listen(3000,() => {
         console.log(`Server is running on port ${PORT}`);
     });
 });
