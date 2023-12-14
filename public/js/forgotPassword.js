5e1410002d3381afadf0fd4396a96f99c47d21d0
bbartika <137605070+bbartika@users.noreply.github.com>
2023-12-14 20:17:01 +0530

    Create forgotPassword.js

---
+++ public/js/forgotPassword.js
@@ -0,0 +1,26 @@
+async function forgetPassword(e){
+    e.preventDefault();
+    
+  try{
+    const details={
+        email: e.target.email.value
+    }
+    const response= await axios.post("/password/forgotpassword", details);
+    if(response.status === 200)     
+    {    
+      
+        alert("Reset Link Has Been Sent To Your Mail!")
+    }
+  }
+  catch(err)
+  {
+    // console.log(err);
+    if(err.response.status== 404)
+    {
+      alert(`${err.response.data.message}`);
+       
+    } else{
+      alert(`${err}`);
+      }
+  }
+}
