2c5ab6fb480dfe323e34aa81495f5d719ee5c16b
bbartika <137605070+bbartika@users.noreply.github.com>
2023-12-14 15:39:39 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,35 @@
-const User=require('../model/user')
-const jwt=require('jsonwebtoken')
-exports.authenticate=async(req,res,next)=>{
-    try{
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const authenticate = async (req, res, next) => {
+    try {
+
         const token=req.header('Authorization');
-        const userPayload= jwt.verify(token,'secretkey');
 
-        const user=await User.findByPk(userPayload.userid)
-        req.user=user
-        next()
+
+        // console.log('the token is --------------------------------------------------------------->',token);
+
         
+        const user =jwt.verify(token,process.env.JWT_SECRET);
 
-    }catch(err){
-        res.status(401).json({ error: 'Authentication failed' });
 
+
+        // console.log('userid is---------------------------------------------------------------->',user.userId);
+        const getUser= await User.findByPk(user.userId)
+            // console.log('=================================>',JSON.stringify(user));
+            req.user=getUser;
+            next();
+
+        
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(401).json({success:false});
+
+        
     }
 
+
+
 }
+module.exports={authenticate };
