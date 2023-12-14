5a57519ded1f5a24e2990746c41a6e9d9ac3d10b
bbartika <137605070+bbartika@users.noreply.github.com>
2023-12-14 17:12:38 +0530

    Create reports.js

---
+++ routes/reports.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const reportsController = require('../controllers/reports');
+
+const authenticatemiddleware = require('../middleware/auth');
+
+const router = express.Router();
+
+router.post('/dailyreport', authenticatemiddleware.authenticate, reportsController.dailyreport);
+
+router.post('/monthreport', authenticatemiddleware.authenticate, reportsController.monthreport);
+
+router.get('/prevReport', authenticatemiddleware.authenticate, reportsController.prevReport);
+
+router.get('/download', authenticatemiddleware.authenticate, reportsController.downloadreport);
+
+module.exports = router;
